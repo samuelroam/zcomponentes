bfcb0d0e8e1106fa0c8a538e73bc25ff3bfd5d65
samuelroam <samuelroam@gmail.com>
2023-12-21 11:18:30 +0100

    permisos 777 a carpeta database (a ver si deja de tocar las pelotas) y avanzada página web tienda

---
+++ html/shop.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="./css/css.css">
+</head>
+<body>
+    <div id="container">
+        <div id="title" class="title flex-center">
+            <h1>Compra con estilo</h1>
+        </div>
+        <div id="contenido" class="">
+            <div id="filtros"class="">
+                Tipo de dispositivo:
+            </div>
+            <div id="zonaTienda"class="flex-center">
+                <?php
+                //bucle para repetir tarjetas
+                for ($i=0;$i<18;++$i) {
+                    print("<div class='tarjeta'>tarjeta $i</div>");
+                };
+                ?>
+                <div class="tarjeta">
+                    <div class="imagen">imagen</div>
+                    <div class="nombre">nombre</div>
+                    <div class="precio">precio</div>
+                    <div class="descripcion">descripcion</div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
